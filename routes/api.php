f8c09fafe422247663337e93a3f3cf69876ac2a3
Antsa-Manuela <manuelarakotoarivony@gmail.com>
2026-03-09 09:50:32 +0300

    commit backend 4

--- routes/api.php
+++ routes/api.php
@@ -6,15 +6,29 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ExcelController;
 use App\Http\Controllers\ImportController;
+use App\Models\User;
 
-// 🔹 PUBLIC
+// 🔹 PUBLIC ROUTES
+
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/register', [RegisterController::class, 'register']);
 
-// 🔹 PROTECTED (après login)
-Route::middleware(['auth:sanctum'])->group(function () {
+// 🔹 TEST : récupérer tous les utilisateurs
+Route::get('/users', function () {
+    return User::all();
+});
+
+
+// 🔹 PROTECTED ROUTES (nécessitent un token Sanctum)
+
+Route::middleware('auth:sanctum')->group(function () {
+
     Route::get('/dashboard/data', [DashboardController::class, 'getDashboardData']);
+
     Route::get('/admin/activities', [DashboardController::class, 'getAdminActivities']);
+
     Route::get('/excel/export/activities', [ExcelController::class, 'exportAdminActivities']);
+
     Route::post('/excel/import/data', [ExcelController::class, 'importNavigationData']);
+
 });
