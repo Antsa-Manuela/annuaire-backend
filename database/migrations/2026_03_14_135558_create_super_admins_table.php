94f7ff18cf62e663434bd3d5cee6bef880eea9fd
Antsa-Manuela <manuelarakotoarivony@gmail.com>
2026-03-14 16:59:24 +0300

    commit backend 12

--- database/migrations/2026_03_14_135558_create_super_admins_table.php
+++ database/migrations/2026_03_14_135558_create_super_admins_table.php
@@ -6,21 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('super_admins', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('cin')->unique();
+            $table->string('password');
+            $table->boolean('is_active')->default(true);
+            $table->rememberToken();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('super_admins');
     }
